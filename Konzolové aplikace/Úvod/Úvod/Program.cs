f46283610349aa1f48973a459d0b4e4ca94661e3
3.D Patka Kryštof <Krystof.Patka@student.sps-cl.cz>
2025-11-20 16:18:29 +0100

    first commit

---
+++ Konzolové aplikace/Úvod/Úvod/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+
+using System;
+using System.Collections.Generic;
+
+class ProgramUkol
+{
+    static void Main()
+    {
+        Console.Write("Zadej výraz: ");
+        string input = Console.ReadLine();
+
+        string[] parts = input.Split(' ');
+        List<string> list = new List<string>();
+
+        double current = double.Parse(parts[0]);
+
+        for (int i = 1; i < parts.Length; i += 2)
+        {
+            string op = parts[i];
+            double num = double.Parse(parts[i + 1]);
+
+            if (op == "*")
+                current *= num;
+            else if (op == "/")
+                current /= num;
+            else
+            {
+                list.Add(current.ToString());
+                list.Add(op);
+                current = num;
+            }
+        }
+
+        list.Add(current.ToString());
+
+        double vysledek = double.Parse(list[0]);
+
+        for (int i = 1; i < list.Count; i += 2)
+        {
+            string op = list[i];
+            double num = double.Parse(list[i + 1]);
+
+            if (op == "+") vysledek += num;
+            else vysledek -= num;
+        }
+
+        Console.WriteLine("Výsledek: " + vysledek);
+
+        Console.ReadLine();
+    }
+}
---
+++ Konzolové aplikace/Úvod/Úvod/funkce.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+void print(params string[] messages)
+{
+    foreach (string message in messages)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+
+
+
+
+string input(params string[] messages)
+{
+    print(messages);
+    return Console.ReadLine();
+}
+
+
+(int, int) getMinMax(params int[] numbers)
+{
+    if (numbers.Length == 0) return (0, 0);
+    int min = numbers[0];
+    int max = numbers[0];
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] < min) min = numbers[i];
+        if (numbers[i] > max) max = numbers[i];
+    }
+    return (min, max); 
+}
+
+
+string name = input("Zadej název souboru");
+(int a,int b) = getMinMax(1, 2, 3, 4, 5, 6, 7);
+print(name, a.ToString(), b.ToString());
